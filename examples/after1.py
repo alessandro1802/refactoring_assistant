6aca166e238d4af427e0d2b18f0023d12a314f7d
Alessandro <oleksandr.yasinskyi@icloud.com>
2024-05-17 18:40:16 +0200

    added examples dir and initial notebook

---
+++ examples/after1.py
@@ -0,0 +1,13 @@
+def c(a: int, b: int) -> int:
+    """
+    Returns the sum of two integers.
+    """
+    s: int = a + b
+    return s
+
+
+x: int = 5
+y: int = 10
+result: int = c(x, y)
+print("Result:", result)
+
---
+++ examples/before1.py
@@ -0,0 +1,8 @@
+def c(a, b):
+    s = a + b
+    return s
+
+x = 5
+y = 10
+result = c(x, y)
+print("Result:", result)
